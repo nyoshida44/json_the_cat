c71c3c3284cc3102f4e9d27bd742415ede7fd4f9
Nicholas Yoshida <nyoshida44@gmail.com>
2021-11-04 19:25:58 +0000

    breedfetcher.js works, add some comments later

---
+++ breedfetcher.js
@@ -0,0 +1,18 @@
+const request = require('request');
+
+const args = process.argv;
+const breedRequest = args.slice(2);
+const catBreed = breedRequest[0];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
+  } 
+  const data = JSON.parse(body);
+  if (data.length === 0) {
+    console.log("Breed was not found");
+    return;
+  }
+  console.log(data[0]['description']);
+});
